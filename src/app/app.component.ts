fb71f866089673dd57d97ed43ca87b0c2ed0c2ad
StreetSmart <maxwindsliver@gmail.com>
2019-07-05 15:35:54 -0400

    added stock tracker app

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+// import { Component } from '@angular/core';
+// import { StocksService, StockInterface } from './services/stocks.service';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   // title = 'stocks';
+//   stocks: Array<StockInterface>;
+
+//   constructor(service: StocksService){
+//     service.load(['AAPL']).subscribe(stocks => {
+//       this.stocks = stocks;
+//     });
+//   }
+// }
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {}
